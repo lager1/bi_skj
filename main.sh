1995919f9f518bed7bb9547ad4039c655562b299
lager1 <13869941+lager1@users.noreply.github.com>
2013-03-06 13:39:06 +0100

    initial commit

---
+++ main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+#
+#   
+#
+#
+#
+#
+#
+#
+
+
+
+#
+# semestralni prace predmetu BI-SKJ, FIT CVUT, 2012, letni semestr
+#
+# Tento skript generuje animaci na zaklade poskytnutych datovych souboru,
+# dale je jeho chovani mozne ovlivnit prepinaci nebo konfiguracnim souborem.
+# Je mozne oboji zaroven, pak maji prednost prepinace.
+#
+
+
+
