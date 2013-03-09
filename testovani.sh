31453ab592630f1c2931631327da6187bdb54efd
lager1 <13869941+lager1@users.noreply.github.com>
2013-03-09 16:25:09 +0100

    add tests, check empty parameters for all options

--- testovani.sh
+++ testovani.sh
@@ -7,6 +7,24 @@
 #
 #
 
+#-------------------------------------------------------------------------------
+# funkce pro vypisovani chyb
+# parametry:
+#   funkce bere libovolny pocet parametru
+# vsechny parametry jsou vypsany na standartni chybovy vystup
+# funkce nasledne ukoncuje cely skript pomoci exit 1
+#-------------------------------------------------------------------------------
+function error()
+{
+  for i in "$@"
+  do
+    echo "ERROR: $i" >&2
+  done
+
+  echo "exitting"
+
+  exit 1;
+}
 
 #-------------------------------------------------------------------------------
 # funkce pro kontrolu prepinacu skriptu
@@ -16,9 +34,31 @@
 #-------------------------------------------------------------------------------
 function switchTest1()
 {
-  
+  eval "$1 -$2 \"\"" 
+  eval "$1 -$2 \'\'"
+#  eval "$1 -$2 \'\`\'"
 }
 
+
+
+#-------------------------------------------------------------------------------
+# main
+#-------------------------------------------------------------------------------
+  # zajima nas pouze prvni prednany argument, vic nic
+
+  ! [[ -e "$1" ]] && error "provided script \"$1\" does not exist"
+  ! [[ -f "$1" ]] && error "provided script \"$1\" is not a regular file"
+  ! [[ -r "$1" ]] && error "provided script \"$1\" cannot be read"
+
+  ! [[ "$1" =~ ^/.*$ ]] && error "provided script has ho be entered with full path"
+
+  TEST="$1"
+  SWITCHES="t X x Y y S T F l g e f n v"
+  
+  for i in ${SWITCHES[@]}
+  do
+    switchTest1 "$TEST" "$i"
+  done
 
 
 
