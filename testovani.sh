3954fc6b19989b97ff4da63bcaebc2877039b344
lager1 <13869941+lager1@users.noreply.github.com>
2013-03-09 15:59:55 +0100

    beginning of the testing script

---
+++ testovani.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# testovaci skript pro semetralni praci z predmetu BI-SKJ, FIT CVUT, 2013, letni semestr
+#
+# skript bere jako parametr nazev skriptu, ktery ma byt otestovan
+#
+#
+
+
+#-------------------------------------------------------------------------------
+# funkce pro kontrolu prepinacu skriptu
+# parametry:
+#   funkce bere jeden argument - prepinac, ktery ma byt testovan
+# funkce predava prepinaci prazdnout hodnotu
+#-------------------------------------------------------------------------------
+function switchTest1()
+{
+  
+}
+
+
+
+
